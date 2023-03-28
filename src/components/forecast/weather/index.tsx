399949611fd4f16e951baa1834d4511de5e22664
WAGG-JSegovia <116173080+WAGG-JSegovia@users.noreply.github.com>
2023-03-28 19:21:36 -0300

    adding changes description

--- src/components/forecast/weather/index.tsx
+++ src/components/forecast/weather/index.tsx
@@ -10,6 +10,8 @@ type Props = {
 };
 
 export default function Weather({ value } : Props) {
+
+    console.log("values", value)
     return (
         <div className="container-forescast">
             {value?.map((e : List_Interface) => (
--- src/constants/text/index.ts
+++ src/constants/text/index.ts
@@ -1,9 +1,11 @@
-export const Description : any = {
+export const Description = {
     "clear sky": "Cielo Despejado",
     "few clouds": "Pocas Nubes",
     "scattered clouds": "Nubes Dispersas",
     "broken clouds": "Nubes Rotas",
     "shower rain": "lluvioso",
+    "light rain": "lluvia ligera",
+    "overcast clouds": "nubes nubladas",
     rain: "lluvia",
     thunderstorm: "Tormenta",
     snow: "Nieve",
